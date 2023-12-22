762db1f5e7ea05739dc02c6ff673eb1d3b67941e
SheminRad <samiei.shemin@gmail.com>
2023-12-22 19:26:38 +0200

    created database

---
+++ dbscript.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+def createTable(dbname):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("PRAGMA foreign_keys = 1")  # check the validity of foreign keys
+
+    c.execute("CREATE TABLE IF NOT EXISTS User("
+              "username TEXT PRIMARY KEY,"
+              "password TEXT,"
+              "fullname TEXT,"
+              "email TEXT,"
+              "telno TEXT)")
+
+    c.execute("CREATE TABLE IF NOT EXISTS Category("
+              "cid INTEGER PRIMARY KEY,"
+              "cname TEXT)")
+
+    c.execute("CREATE TABLE IF NOT EXISTS Advertisement("
+              "aid INTEGER PRIMARY KEY AUTOINCREMENT,"
+              "title TEXT,"
+              "description TEXT,"
+              "isactive INTEGER,"
+              "make TEXT,"
+              "belongto INTEGER,"
+              "FOREIGN KEY (make) REFERENCES User(username),"
+              "FOREIGN KEY (belongto) REFERENCES Category(cid))")
+
+    conn.commit()
+    conn.close()
+
+def insert(dbname):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    category = [(1,"Clothes"), (2,"Technology"), (3, "Cars"), (4, "Food"), (5, "Drink")]
+    c.executemany("INSERT INTO Category VALUES(?,?)", category)
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    createTable("adv.db")
+    insert("adv.db")
