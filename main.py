5c374aa6eb801d5a6bfe9ed76c5d57ef6f73ad81
SheminRad <samiei.shemin@gmail.com>
2023-12-26 00:46:23 +0200

    updated registration

--- main.py
+++ main.py
@@ -4,35 +4,53 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "123"
 
-@app.route("/register")
-def index():
-    if "username" and "password" in session:
-        # dictionary in python flask, username and password as a key in this dict
-        return render_template("login.html",
-                               username=session["username"], password=session["password"])
-        # if username available: send username to the login.html
-    else:
-        return render_template("register.html")
-        # register screen will be displayed if no username is available
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        fullname = request.form["Full name"]
+        email = request.form["Email address"]
+        tel = request.form["Telephone number"]
+
+        # to be completed: Update database
+        # conn = sqlite3.connect("adv.db")
+        # c = conn.cursor()
+
+        return redirect(url_for('register_success'))
+    else:
+        return render_template('register.html')
+
+@app.route("/register_success")
+def register_success():
+    return render_template('register_success.html')
+
+
+@app.route("/login", methods=["POST"])
 def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
         # now connect to db and check username and password and update the session dictionary
         conn = sqlite3.connect("adv.db")
         c = conn.cursor()
-        c.execute("SELECT * FROM User WHERE username = ? AND password = ?", (username, password))
+        c.execute("SELECT * FROM User WHERE username = ? AND password = ?",
+                  (username, password))
         row = c.fetchone()
         conn.close()
+
+
         if row != None:
             # if the username exists in our db
             session["username"] = username
-            session["password"] = password
-        # else:
-        #     # redirect
-        #OR do this instead
+
         return redirect(url_for('login'))
         # go to login function
 
@@ -40,9 +58,8 @@ def login():
         pass
 @app.route('/logout')
 def logout():
-    # remove the username and password from our session
+    # remove the username from our session
     session.pop("username", None)
-    session.pop("password", None)
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
